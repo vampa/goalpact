9769c1e0e4a041f496a15e880a13116da4988023
Alex Stomp <stompy1208@gmail.com>
2014-10-28 18:45:04 -0500

    redesign for desktop ui

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,10 +1,10 @@
 class GoalsController < ApplicationController
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    before_action :authenticate_user!
 
-  def new
+    def new
 	end
 
 	def create
