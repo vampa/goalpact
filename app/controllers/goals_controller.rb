18b4184f0521128d0a68de8a25c8ea27ec1b719c
Alex Stomp <stompy1208@gmail.com>
2014-11-07 22:02:56 -0600

    adds a bit to the dashboard + new goal + icomoon font

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,11 +3,17 @@ class GoalsController < ApplicationController
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
     before_action :authenticate_user!
+    
+    # Only goal page that won't use the dash layout is the public view
+    layout 'dash', :except => [:public]
 
     def new
 	end
 
 	def create
 	end
+
+    def public
+    end
 
 end
